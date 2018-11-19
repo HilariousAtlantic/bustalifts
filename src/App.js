a47c6bdbe4b54911721baa9582dc8838170e5043
Luke Artnak <lartnak@gmail.com>
2018-11-18 22:20:19 -0500

    update to hooks version

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import React, { Component } from "react";
+import React from "react";
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <BrowserRouter>
-          <Switch>
-            <Route exact path="/workouts" component={WorkoutList} />
-            <Route exact path="/workouts/:id" component={Workout} />
-            <Redirect to="/workouts" />
-          </Switch>
-        </BrowserRouter>
-      </div>
-    );
-  }
+export default function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/workouts" component={WorkoutList} />
+          <Route exact path="/workouts/:id" component={Workout} />
+          <Redirect to="/workouts" />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 function WorkoutList() {
@@ -25,5 +23,3 @@ function WorkoutList() {
 function Workout() {
   return <div />;
 }
-
-export default App;
