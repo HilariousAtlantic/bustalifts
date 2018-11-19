617778871d109031ba9d342ad220418c634786ec
Luke Artnak <lartnak@gmail.com>
2018-11-18 22:59:49 -0500

    add styled components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 import React from "react";
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import { StateProvider } from "./state";
-import "./App.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <StateProvider>
-        <BrowserRouter>
-          <Switch>
-            <Route exact path="/workouts" component={WorkoutList} />
-            <Route exact path="/workouts/:id" component={Workout} />
-            <Redirect to="/workouts" />
-          </Switch>
-        </BrowserRouter>
-      </StateProvider>
-    </div>
+    <StateProvider>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/workouts" component={WorkoutList} />
+          <Route exact path="/workouts/:id" component={Workout} />
+          <Redirect to="/workouts" />
+        </Switch>
+      </BrowserRouter>
+    </StateProvider>
   );
 }
 
